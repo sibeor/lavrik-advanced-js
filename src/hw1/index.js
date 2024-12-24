2571b429e8596ba419bb89b332dcd811ca7d0bf4
Andrei Timotin <timotin.andrei@justconsult.md>
2024-12-24 12:09:47 +0200

    feat: hw1

--- src/hw1/index.js
+++ src/hw1/index.js
@@ -1,62 +1,51 @@
-const STORAGE_PROPABILITY = 1;
-const CALC_PROPABILITY = 1;
+const STORAGE_PROPABILITY = 0.8
+const CALC_PROPABILITY = 0.7
 
-function getItemsFromDB(){
-	if(Math.random() > STORAGE_PROPABILITY){
-		return null; // ошибка хранилища
-	}
-
-	return [
-		{ id: 5, price: 1000 },
-		{ id: 7, price: 2000 },
-		{ id: 8, price: 4000 }
-	]
+function getItemsFromDB () {
+  if (Math.random() > STORAGE_PROPABILITY) {
+    throw new CalcError(`Storage Error: cant get items from DB`)
+    // return null; // ошибка хранилища
+  }
+  
+  return [
+    { id: 5, price: 1000 },
+    { id: 7, price: 2000 },
+    { id: 8, price: 4000 }
+  ]
 }
 
-function calcTotal(items){
-	if(Math.random() > CALC_PROPABILITY){
-		return null; // ошибка рассчёта
-	}
-
-	return items.reduce((total, item) => {
-		return total + item.price;
-	}, 0);
+function calcTotal (items) {
+  if (Math.random() > CALC_PROPABILITY) {
+    throw new CalcError(`Calc Error: cant get total of items`)
+    // return null; // ошибка рассчёта
+  }
+  
+  return items.reduce((total, item) => {
+    return total + item.price
+  }, 0)
 }
 
-function calcYearBalance(total){
-	if(Math.random() > CALC_PROPABILITY){
-		return null; // ошибка рассчёта
-	}
-
-	return Math.floor(total * Math.random());
+function calcYearBalance (total) {
+  if (Math.random() > CALC_PROPABILITY) {
+    throw new CalcError(`Calc Error: cant get balance of total`)
+    // return null; // ошибка рассчёта
+  }
+  
+  return Math.floor(total * Math.random())
 }
 
-function main(){
-	let items = getItemsFromDB();
-
-	if(items === null){
-		document.body.innerHTML = `Storage Error: cant get items from DB`;
-	}
-	else{
-		let total = calcTotal(items);
-		
-		if(total === null){
-			document.body.innerHTML = `Calc Error: cant get total of items`;
-		}
-		else{
-			let balance = calcYearBalance(total);
-
-			if(balance === null){
-				document.body.innerHTML = `Calc Error: cant get balanse of total`;
-			}
-			else{
-				document.body.innerHTML = `Nice! ${balance}`;
-			}
-		}
-	}
+function main () {
+  
+  try {
+    let items = getItemsFromDB()
+    let total = calcTotal(items)
+    let balance = calcYearBalance(total)
+    document.body.innerHTML = `Nice! ${balance}`
+  } catch (e) {
+    document.body.innerHTML = `${e.message}`
+  }
 }
 
-main();
+class CalcError extends Error {}
 
-/* class CalcError extends Error{};
-class StorageError extends Error{}; */
+main()
